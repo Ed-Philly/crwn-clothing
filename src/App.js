4afcd163d81b7e55093e61fa246688023f7b0579
Ed philly <osas.edok@yahoo.co.uk>
2020-04-14 01:50:34 +0100

    update our menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component'
+import { Switch, Route, Link } from 'react-router-dom';
 
 import './App.css';
 
+const HatsPage = () => (
+  <div
+    style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', height: '340px', margin: '25px 25px', backgroundColor: 'lightblue' }}>
+    <h1>HATS PAGE</h1></div>
+)
+
 function App() {
   return (
-    <HomePage />
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop/hats' component={HatsPage} />
+
+      </Switch>
+    </div>
+
+
   );
 }
 
--- src/components/menu-item/menu-item.component.js
+++ src/components/menu-item/menu-item.component.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './menu-item.styles.scss'
+import { withRouter } from 'react-router-dom';
 
-const MenuItem = ({ title, imageurl, size }) => (
-    <div className={`${size} menu-item`}>
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+    <div
+        className={`${size} menu-item`}
+        onClick={() => history.push(`${match.url}${linkUrl}`)}>
 
         <div
             className='background-image'
             style={{
-                backgroundImage: `url(${imageurl})`
+                backgroundImage: `url(${imageUrl})`
             }}
         ></div>
 
@@ -21,4 +24,4 @@ const MenuItem = ({ title, imageurl, size }) => (
 
 )
 
-export default MenuItem;
+export default withRouter(MenuItem);
