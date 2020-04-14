988aede8f12fb242a3d422a5bc7c8d1dfb6e7460
Ed philly <osas.edok@yahoo.co.uk>
2020-04-14 04:40:40 +0100

    Adding Header componet and nav links

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
 import { Switch, Route, Link } from 'react-router-dom';
+import Header from './components/header/header.component'
 
 import './App.css';
 
@@ -14,6 +15,7 @@ const HatsPage = () => (
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop/' component={ShopPage} />
